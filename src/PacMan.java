b329362ba870a30248c931522d76814ff3ac373c
thoup99 <thoup99@outlook.com>
2025-01-28 19:52:24 -0500

    Init

---
+++ src/PacMan.java
@@ -0,0 +1,37 @@
+import game.GamePanel;
+import keyboard.KeyHandler;
+
+import javax.swing.*;
+
+/**
+ * Tetris.java
+ * Class used to run PacMan.
+ *
+ * @author Tyler Houp
+ */
+public class PacMan {
+    /**
+     * Initializes KeyHandler and Audio Player then creates
+     * JFrame for the game to be displayed on. Creates gamePanel
+     * and sets settings on it. Finally, gamePanel starts the gameThread.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        KeyHandler.initialize();
+
+        JFrame frame = new JFrame("Tetris");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
